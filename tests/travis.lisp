320904daa00f8ed5d7128561ec9ef0f545a39c4c
Vasily Postnicov <shamaz.mazum@gmail.com>
2020-01-06 09:44:38 +0300

    Fix travis scripts

--- tests/travis.lisp
+++ tests/travis.lisp
@@ -22,7 +22,7 @@
   (error 'not-implemented :proc (list 'quit code)))
 
 (defun do-all()
-  (asdf:load-system :wavelet-audio-tests)
+  (asdf:load-system :wavelet-audio/tests)
   (portable-quit
    (if (funcall
         (intern (symbol-name :run-tests)
