3626b846fc3f91a9114104dcca0ad978e84a8f83
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-02-04 16:27:17 +0300

    Check if compilation is successful

--- tests/travis.lisp
+++ tests/travis.lisp
@@ -1,5 +1,8 @@
 (defun do-all()
-  (ql:quickload :wavelet-audio/tests)
+  (handler-case
+      (ql:quickload :wavelet-audio/tests)
+    (asdf:compile-file-error ()
+      (uiop:quit 1)))
   (uiop:quit
    (if (uiop:call-function "wavelet-audio-tests:run-tests")
        0 1)))
